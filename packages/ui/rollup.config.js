67217ff21c9a51419e8b7bed27db96a8a2510158
Yarn <ztytotoro@users.noreply.github.com>
2020-09-19 09:42:05 +0800

    chore: add outputs

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -78,4 +78,40 @@ export default [
       terser(),
     ],
   },
+  {
+    input: 'src/index.ts',
+    output: {
+      sourcemap: true,
+      file: 'lib/index.full.esm.js',
+      format: 'esm',
+    },
+    plugins: [
+      svelte({
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        //   css: (css) => {
+        //     css.write('bundle.css');
+        //   },
+        preprocess: sveltePreprocess(),
+        customElement: true,
+      }),
+
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        web: true,
+        dedupe: ['svelte'],
+      }),
+      commonjs(),
+      typescript({
+        // sourceMap: !production,
+        // inlineSources: !production,
+      }),
+
+      terser(),
+    ],
+  },
 ];
