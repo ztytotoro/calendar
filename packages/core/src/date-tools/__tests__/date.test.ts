15a7efc9f9b71ed4f09c4393771ff7ea80ca5f04
Yarn <ztytotoro@users.noreply.github.com>
2020-09-27 23:25:17 +0800

    chore: update

--- packages/core/src/date-tools/__tests__/date.test.ts
+++ packages/core/src/date-tools/__tests__/date.test.ts
@@ -1,8 +1,19 @@
-import { dayOfYear, normalDate } from '../date';
+import { dayOfYear, eraseTime, normalDate } from '../date';
 
 describe('date.ts', () => {
-  test('dayOfYear', () => {
-    expect(dayOfYear(normalDate(2020, 1, 30))).toBe(30);
-    expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
-  });
+    test('normalDate', () => {
+        expect(normalDate(2020, 1, 30)).toEqual(new Date('2020-1-30'));
+    });
+
+    test('dayOfYear', () => {
+        expect(dayOfYear(normalDate(2020, 1, 30))).toBe(30);
+        expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
+    });
+
+    test('eraseTime', () => {
+        expect(eraseTime(new Date('2020-10-1 10:00:00'))).toEqual(
+            new Date('2020-10-1 00:00:00')
+        );
+        expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
+    });
 });
