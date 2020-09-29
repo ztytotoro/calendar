f3a0e4ae2c4ad4d3ddcf196843982e5bd99c82fd
Tianyang Zhou <tzu@mm-software.com>
2020-09-29 15:32:54 +0800

    test: update test files

--- packages/core/src/date-tools/__tests__/date.test.ts
+++ packages/core/src/date-tools/__tests__/date.test.ts
@@ -1,19 +1,26 @@
-import { dayOfYear, eraseTime, normalDate } from '../date';
+import { dayOfYear, eraseTime, addWeekDays, normalDate } from '../date';
 
 describe('date.ts', () => {
-    test('normalDate', () => {
-        expect(normalDate(2020, 1, 30)).toEqual(new Date('2020-1-30'));
-    });
+  test('normalDate', () => {
+    expect(normalDate(2020, 1, 30)).toEqual(new Date('2020-1-30'));
+  });
 
-    test('dayOfYear', () => {
-        expect(dayOfYear(normalDate(2020, 1, 30))).toBe(30);
-        expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
-    });
+  test('dayOfYear', () => {
+    expect(dayOfYear(normalDate(2020, 1, 30))).toBe(30);
+    expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
+  });
 
-    test('eraseTime', () => {
-        expect(eraseTime(new Date('2020-10-1 10:00:00'))).toEqual(
-            new Date('2020-10-1 00:00:00')
-        );
-        expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
-    });
+  test('eraseTime', () => {
+    expect(eraseTime(new Date('2020-10-1 10:00:00'))).toEqual(
+      new Date('2020-10-1 00:00:00')
+    );
+    expect(dayOfYear(normalDate(2020, 2, 5))).toBe(36);
+  });
+
+  test('nextWeekDay', () => {
+    expect(addWeekDays(7, 1)).toBe(1);
+    expect(addWeekDays(7, -1)).toBe(6);
+    expect(addWeekDays(7, -7)).toBe(7);
+    expect(addWeekDays(7, 8)).toBe(1);
+  });
 });
